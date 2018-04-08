faeafa939f5fd2c5e4b6df002ab6820122723d1b
Adrian <adrianw@gmail.com>
2018-04-08 18:11:24 +0100

    init commit

---
+++ src/main/java/com/aweglicki/app/SpringGymApplication.java
@@ -0,0 +1,12 @@
+package com.aweglicki.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGymApplication.class, args);
+	}
+}
